9480d894a8aec0ffd36b77592834af8629529d77
Leon Liu <lyliu@walmart.com>
2012-12-26 15:34:30 +0800

    add basic module

---
+++ basic/src/main/java/org/huamuzhen/codewarehouse/basic/StaticFieldExample.java
@@ -0,0 +1,32 @@
+package org.huamuzhen.codewarehouse.basic;
+
+/**
+ * check the running sequence of static field
+ * and Constructor. static block run first
+ * */
+public class StaticFieldExample {
+	
+	public static int b=1;
+
+	public StaticFieldExample(){
+		System.out.println("constructor");
+	}
+	
+	static{
+		a=2;
+		b=2;
+		System.out.println("static 1");
+	}
+	
+	public static int a =1;
+	
+	static{
+		System.out.println("static 2");
+	}
+	
+	public static void main(String args[]){
+		StaticFieldExample sfe = new StaticFieldExample();
+		System.out.println("a="+ a);
+		System.out.println("b="+ b);
+	}
+}
